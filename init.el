62dd3bb4cf19a41a68e167642b594edaeccf03fb
Clement TROUILLARD <ctrouill@student.42.fr>
2017-11-20 17:52:12 +0100

    Added severa paths

--- init.el
+++ init.el
@@ -7,3 +7,29 @@
   "Set global config path folder to load"
   (expand-file-name (convert-standard-filename "lisp")
 					user-emacs-directory))
+
+(defvar *cfg-core*
+  "Subfolder config path that contains core
+   configurations"
+  (concat (file-name-as-directory *config-dir*) "core"))
+(defvar *cfg-pkg*
+  "Subfolder config path that contains package
+   configurations"
+  (concat (file-name-as-directory *config-dir*) "package"))
+(defvar *cfg-langs*
+  "Subfolder config path that contains langages
+   related config files"
+  (concat (file-name-as-directory *config-dir*) "languages"))
+(defvar *cfg-exp*
+  "Playground folder where some features can breaks."
+  (concat (file-name-as-directory *config-dir*) "experimental"))
+
+(defvar *defun-dir*
+  "Custom declared functions or tricks are stored here"
+  (expand-file-name (convert-standard-filename "defun")
+					user-emacs-directory))
+(defvar *plugins-dir*
+  "One file external plugins stored here are  loaded
+   at emacs's boot"
+  (expand-file-name (convert-standard-filename "plugins")
+					user-emacs-directory))
