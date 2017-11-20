62a9a95972da4f461eafd38ede7b85df8399aaed
Clement TROUILLARD <ctrouill@student.42.fr>
2017-11-20 18:20:36 +0100

    Added loader

--- init.el
+++ init.el
@@ -11,18 +11,22 @@
 (defvar *cfg-core*
   "Subfolder config path that contains core
    configurations"
-  (concat (file-name-as-directory *config-dir*) "core"))
+  (concat (file-name-as-directory *config-dir*)
+		  "core"))
 (defvar *cfg-pkg*
   "Subfolder config path that contains package
    configurations"
-  (concat (file-name-as-directory *config-dir*) "package"))
+  (concat (file-name-as-directory *config-dir*)
+		  "package"))
 (defvar *cfg-langs*
   "Subfolder config path that contains langages
    related config files"
-  (concat (file-name-as-directory *config-dir*) "languages"))
+  (concat (file-name-as-directory *config-dir*)
+		  "languages"))
 (defvar *cfg-exp*
   "Playground folder where some features can breaks."
-  (concat (file-name-as-directory *config-dir*) "experimental"))
+  (concat (file-name-as-directory *config-dir*)
+		  "experimental"))
 
 (defvar *defun-dir*
   "Custom declared functions or tricks are stored here"
@@ -33,3 +37,14 @@
    at emacs's boot"
   (expand-file-name (convert-standard-filename "plugins")
 					user-emacs-directory))
+
+(defun load-config-folders (config-folders)
+  "For each folder in emacs's path, loading"
+  (if (file-exists-p config-folders)
+	  (progn (add-to-list 'load-path config-folders)
+			 (mapc (lambda (fname)
+					 (let ((feat (intern (file-name-base fname))))
+					   (message "Feature '%s' wijze." feat)
+					   (require feat fname)))
+				   (directory-files config-folders t "\\.el")))
+	(message "Directory \"%s\" niet gevonden. Geen extensies zijn geladen." config-folders)))
