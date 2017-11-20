136e7d0f0a7ec63b4e55370773964d122f8a2adf
Clement TROUILLARD <ctrouill@student.42.fr>
2017-11-20 17:31:02 +0100

    Fixed backup

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; File:     ~/.emacs.d/emacs-init.el
+;; Author:   iomonad <me@trosa.io>
 ;; https://github.com/iomonad/iomonad.el
-;; Copyright (c) iomonad <me@trosa.io>
 ;;
+
+(defvar *config-dir*
+  "Set global config path folder to load"
+  (expand-file-name (convert-standard-filename "lisp")
+					user-emacs-directory))
