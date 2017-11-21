a98ae4cdd75f0f205764cdf45e46a0030330a82d
Clement Trosa <me@trosa.io>
2017-11-22 00:10:41 +0100

    stopped caring 23 commits ago

--- lisp/package/pkg-company.el
+++ lisp/package/pkg-company.el
@@ -1,7 +1,12 @@
 ;; -*- lexical-binding: t -*-
-(use-package company
-  :ensure t)
 
-(add-hook 'after-init-hook 'global-company-mode)
+(use-package company :ensure t
+  :init (setq company-auto-complete nil
+			  company-tooltip-flip-when-above t
+			  company-minimum-prefix-length 2
+			  company-tooltip-limit 20
+			  company-idle-delay 0.5)
+  :config (global-company-mode 1)
+  :diminish company-mode)
 
 (provide 'pkg-company)
