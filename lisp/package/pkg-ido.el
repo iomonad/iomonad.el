a51d6b97d5c3888b15d34757d47c3c8d43dd31d8
Clement TROUILLARD <ctrouill@student.42.fr>
2017-11-22 11:05:30 +0100

    It compiles! Ship it!

--- lisp/package/pkg-ido.el
+++ lisp/package/pkg-ido.el
@@ -4,14 +4,27 @@
 
 (use-package ido :ensure t
   :init (setq ido-case-fold t
-              ido-enable-flex-matching t
-              ido-use-filename-at-point 'guess
-              ido-create-new-buffer 'always)
+			  ido-enable-flex-matching t
+			  ido-use-filename-at-point 'guess
+			  ido-create-new-buffer 'always)
   :config (progn
-            (ido-everywhere 1)
-            (flx-ido-mode 1)
-            (ido-mode t)
-            (ido-hacks-mode)
-            (ido-vertical-mode)))
+			(ido-everywhere 1)
+			(flx-ido-mode 1)
+			(ido-mode t)
+			(ido-hacks-mode)
+			(ido-vertical-mode)))
+
+(setq ido-file-extensions-order '(".org" ".tex" ".el" ".txt" ".c" ".cpp" ".h" ".lisp"))
+
+
+(setq flx-ido-use-faces t) ;; enable flx highlights
+(flx-ido-mode 1)
+
+(mapc (lambda (x)
+		(add-to-list 'ido-ignore-directories x))
+	  (list "target" "node_modules" "out"))
+(mapc (lambda (x)
+		(add-to-list 'ido-ignore-files x))
+	  (list "\.bak" "\.o" "\.elc" "#*#" "*~" "\.a"))
 
 (provide 'pkg-ido)
--- lisp/package/pkg-undotree.el
+++ lisp/package/pkg-undotree.el
@@ -5,4 +5,20 @@
 (global-undo-tree-mode)
 (defalias 'redo 'undo-tree-redo)
 
+(define-key neotree-mode-map (kbd "h") 'neotree-enter-horizontal-split)
+(define-key neotree-mode-map (kbd "v") 'neotree-enter-vertical-split)
+
+(define-key evil-motion-state-local-map (kbd "?")    'evil-search-backward)
+(define-key evil-motion-state-local-map (kbd "c")    'neotree-create-node)
+(define-key evil-motion-state-local-map (kbd "d")    'neotree-delete-node)
+(define-key evil-motion-state-local-map (kbd "gr")   'neotree-refresh)
+(define-key evil-motion-state-local-map (kbd "H")    'neotree-select-previous-sibling-node)
+(define-key evil-motion-state-local-map (kbd "J")    'neotree-select-down-node)
+(define-key evil-motion-state-local-map (kbd "K")    'neotree-select-up-node)
+(define-key evil-motion-state-local-map (kbd "L")    'neotree-select-next-sibling-node)
+(define-key evil-motion-state-local-map (kbd "q")    'neotree-hide)
+(define-key evil-motion-state-local-map (kbd "r")    'neotree-rename-node)
+(define-key evil-motion-state-local-map (kbd "R")    'neotree-change-root)
+(define-key evil-motion-state-local-map (kbd "s")    'neotree-hidden-file-toggle)
+
 (provide 'pkg-undotree)
