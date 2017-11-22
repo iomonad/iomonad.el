551f84ae3529b96b15ba0fff543119ab287a9505
Clement Trosa <me@trosa.io>
2017-11-22 23:14:58 +0100

    QuickFix.

--- lisp/package/pkg-evil.el
+++ lisp/package/pkg-evil.el
@@ -49,5 +49,11 @@
 (define-key evil-normal-state-map (kbd "C-a") 'move-beginning-of-line)
 (define-key evil-normal-state-map (kbd "C-e") 'move-end-of-line)
 
+(define-key evil-insert-state-map (kbd "C-a") 'move-beginning-of-line)
+(define-key evil-insert-state-map (kbd "C-e") 'move-end-of-line)
+
+(define-key evil-insert-state-map (kbd "C-k") 'kill-line)
+(define-key evil-normal-state-map (kbd "C-k") 'kll-line)
+
 
 (provide 'pkg-evil)
