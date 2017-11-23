1f72642b55505661990586de9dc65f1b74d01cae
Clement TROUILLARD <ctrouill@student.42.fr>
2017-11-23 11:30:25 +0100

    I don't know what these changes are supposed to accomplish but somebody told me to make them.

--- lisp/package/pkg-helm.el
+++ lisp/package/pkg-helm.el
@@ -10,4 +10,12 @@
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 
+(defun helm-mini ()
+  "Preconfigured `helm' lightweight version \(buffer -> recentf\)."
+  (interactive)
+  (helm-other-buffer '(helm-c-source-buffers-list
+                       helm-c-source-recentf
+                       helm-c-source-buffer-not-found)
+					 "*helm mini*"))
+
 (provide 'pkg-helm)
