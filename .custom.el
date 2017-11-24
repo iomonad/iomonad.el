33c4fdcbf0c50061c786218a2b34fd586780f3b6
Clement TROUILLARD <ctrouill@student.42.fr>
2017-11-24 14:22:52 +0100

    fixed conflicts (LOL merge -s ours; push -f)

--- .custom.el
+++ .custom.el
@@ -5,10 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(rainbow-delimiters clojure-mode gh request yasnippet neotree magit helm-google helm evil-nerd-commenter evil-leader evil company use-package))))
+    (all-the-icons rainbow-delimiters clojure-mode gh request yasnippet neotree magit helm-google helm evil-nerd-commenter evil-leader evil company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line-inactive ((t (:background nil))))
+ '(neo-dir-link-face ((t (:foreground "white"))))
+ '(neo-file-link-face ((t (:foreground "white")))))
--- lisp/package/pkg-neotree.el
+++ lisp/package/pkg-neotree.el
@@ -2,4 +2,20 @@
 (use-package neotree
   :ensure t)
 
+(use-package all-the-icons
+  :ensure t)
+
+(setq neo-theme
+      (if (display-graphic-p) 'icons 'arrow))
+
+(custom-set-faces
+ '(neo-dir-link-face ((t (:foreground "white"))))
+ '(neo-file-link-face ((t (:foreground "white")))))
+
+(evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+(evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+(evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+(evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+
+
 (provide 'pkg-neotree)
