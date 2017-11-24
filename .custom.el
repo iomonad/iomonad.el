53eb568c603ae6343c5314872e6f670ab4d376a9
Clement TROUILLARD <ctrouill@student.42.fr>
2017-11-24 13:17:02 +0100

    I am even stupider than I thought

---
+++ .custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(rainbow-delimiters clojure-mode gh request yasnippet neotree magit helm-google helm evil-nerd-commenter evil-leader evil company use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
