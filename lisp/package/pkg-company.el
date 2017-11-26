9e75fe1a1c3503181adde5e199ffddb581948a09
Clement Trosa <me@trosa.io>
2017-11-26 18:59:19 +0100

    Glue. Match sticks. Paper. Build script!

--- lisp/package/pkg-company.el
+++ lisp/package/pkg-company.el
@@ -1,12 +1,21 @@
 ;; -*- lexical-binding: t -*-
+(require 'color)
 
 (use-package company :ensure t
   :init (setq company-auto-complete nil
-			  company-tooltip-flip-when-above t
-			  company-minimum-prefix-length 2
-			  company-tooltip-limit 20
-			  company-idle-delay 0.5)
+              company-tooltip-flip-when-above t
+              company-minimum-prefix-length 2
+              company-tooltip-limit 20
+              company-idle-delay 0.5)
   :config (global-company-mode 1)
   :diminish company-mode)
+
+(let ((bg (face-attribute 'default :background)))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
 
 (provide 'pkg-company)
--- lisp/package/pkg-evil.el
+++ lisp/package/pkg-evil.el
@@ -101,98 +101,98 @@
 (setq powerline-text-scale-factor 1)
 
 (defun ml/fill (face reserve)
-	"Return empty space using FACE and leaving RESERVE space on the right."
-	(unless reserve
-		(setq reserve 20))
-	(when powerline-text-scale-factor
-		(setq reserve (* powerline-text-scale-factor reserve)))
-	(when (and window-system (eq 'right (get-scroll-bar-mode)))
-		(setq reserve (- reserve 3)))
-	(propertize " "
-							'display `((space :align-to (- (+ right right-fringe right-margin) ,reserve)))
-							'face face))
+  "Return empty space using FACE and leaving RESERVE space on the right."
+  (unless reserve
+    (setq reserve 20))
+  (when powerline-text-scale-factor
+    (setq reserve (* powerline-text-scale-factor reserve)))
+  (when (and window-system (eq 'right (get-scroll-bar-mode)))
+    (setq reserve (- reserve 3)))
+  (propertize " "
+              'display `((space :align-to (- (+ right right-fringe right-margin) ,reserve)))
+              'face face))
 (defun ml/width (values)
-	"Get the length of VALUES."
-	(if values
-			(let ((val (car values)))
-				(+ (cond
-						((stringp val) (length (format-mode-line val)))
-						((and (listp val) (eq 'image (car val)))
-						 (car (image-size val)))
-						(t 0))
-					 (ml/width (cdr values))))
-		0))
+  "Get the length of VALUES."
+  (if values
+      (let ((val (car values)))
+        (+ (cond
+            ((stringp val) (length (format-mode-line val)))
+            ((and (listp val) (eq 'image (car val)))
+             (car (image-size val)))
+            (t 0))
+           (ml/width (cdr values))))
+    0))
 (defun ml/item-render (item)
-	"Render a powerline ITEM."
-	(cond
-	 ((and (listp item) (eq 'image (car item)))
-		(propertize " " 'display item
-								'face (plist-get (cdr item) :face)))
-	 (item item)))
+  "Render a powerline ITEM."
+  (cond
+   ((and (listp item) (eq 'image (car item)))
+    (propertize " " 'display item
+                'face (plist-get (cdr item) :face)))
+   (item item)))
 (defun ml/render (values)
-	"Render a list of powerline VALUES."
-	(mapconcat 'ml/item-render values ""))
+  "Render a list of powerline VALUES."
+  (mapconcat 'ml/item-render values ""))
 (defun ml/format (value fontface)
-	(propertize (format-mode-line value) 'face fontface))
+  (propertize (format-mode-line value) 'face fontface))
 (defun ml/column ()
-	(propertize "%c" 'face
-							(if (>= (current-column) 80)
-									'mode-line-80col
-								'mode-line-col)))
+  (propertize "%c" 'face
+              (if (>= (current-column) 80)
+                  'mode-line-80col
+                'mode-line-col)))
 (defun makemodeline ()
-	(setq-default mode-line-format
-								'(""
-									(:eval
-									 (let* ((lhs (list
-																(ml/format evil-mode-line-msg 'mode-line-evil)
-																" ** "
-																(ml/format "%b" 'mode-line-buffer-name)
-																" ~ "
-																(ml/format mode-name 'mode-line-mode)
-																(ml/format minor-mode-alist 'mode-line-minor-mode)))
-													(rhs (list
-																(ml/format which-func-format 'mode-line-func)
-																" :: "
-																(ml/format "%4l:" 'mode-line-line)
-																(ml/column))))
-										 (concat (ml/render lhs)
-														 (ml/fill 'mode-line-line-face (ml/width rhs))
-														 (ml/render rhs)))))))
+  (setq-default mode-line-format
+                '(""
+                  (:eval
+                   (let* ((lhs (list
+                                (ml/format evil-mode-line-msg 'mode-line-evil)
+                                " ** "
+                                (ml/format "%b" 'mode-line-buffer-name)
+                                " ~ "
+                                (ml/format mode-name 'mode-line-mode)
+                                (ml/format minor-mode-alist 'mode-line-minor-mode)))
+                          (rhs (list
+                                (ml/format which-func-format 'mode-line-func)
+                                " :: "
+                                (ml/format "%4l:" 'mode-line-line)
+                                (ml/column))))
+                     (concat (ml/render lhs)
+                             (ml/fill 'mode-line-line-face (ml/width rhs))
+                             (ml/render rhs)))))))
 (makemodeline)
 (defvar mode-line-cleaner-alist
-	`((auto-complete-mode . "")
-		(yas-minor-mode . "")
-		(smartparens-mode . " Φ")
-		(eldoc-mode . "")
-		(abbrev-mode . "")
-		(undo-tree-mode . "")
-		(volatile-highlights-mode . " υ")
-		(elisp-slime-nav-mode . " δ")
-		(nrepl-mode . " ηζ")
-		(nrepl-interaction-mode . " ηζ")
-		(git-gutter+-mode . "")
-		;; Major modes
-		(clojure-mode . "λ")
-		(hi-lock-mode . "")
-		(python-mode . "Py")
-		(emacs-lisp-mode . "ELisp")
-		(markdown-mode . "MDown"))
-	"Alist for `clean-mode-line'.
+  `((auto-complete-mode . "")
+    (yas-minor-mode . "")
+    (smartparens-mode . " Φ")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    (undo-tree-mode . "")
+    (volatile-highlights-mode . " υ")
+    (elisp-slime-nav-mode . " δ")
+    (nrepl-mode . " ηζ")
+    (nrepl-interaction-mode . " ηζ")
+    (git-gutter+-mode . "")
+    ;; Major modes
+    (clojure-mode . "λ")
+    (hi-lock-mode . "")
+    (python-mode . "Py")
+    (emacs-lisp-mode . "ξ Lisp")
+    (markdown-mode . "MDown"))
+  "Alist for `clean-mode-line'.
 When you add a new element to the alist, keep in mind that you
 must pass the correct minor/major mode symbol and a string you
 want to use in the modeline *in lieu of* the original.")
 
 (defun clean-mode-line ()
-	(interactive)
-	(loop for cleaner in mode-line-cleaner-alist
-				do (let* ((mode (car cleaner))
-									(mode-str (cdr cleaner))
-									(old-mode-str (cdr (assq mode minor-mode-alist))))
-						 (when old-mode-str
-							 (setcar old-mode-str mode-str))
-						 ;; major mode
-						 (when (eq mode major-mode)
-							 (setq mode-name mode-str)))))
+  (interactive)
+  (loop for cleaner in mode-line-cleaner-alist
+        do (let* ((mode (car cleaner))
+                  (mode-str (cdr cleaner))
+                  (old-mode-str (cdr (assq mode minor-mode-alist))))
+             (when old-mode-str
+               (setcar old-mode-str mode-str))
+             ;; major mode
+             (when (eq mode major-mode)
+               (setq mode-name mode-str)))))
 
 (add-hook 'after-change-major-mode-hook 'clean-mode-line)
 
